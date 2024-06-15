66db736c037ed8b9fb408a27fbb1d8de5e1100dc
Zoid <satriaalfa77@gmail.com>
2024-06-15 14:31:01 +0700

    update type data

--- typedata-math.py
+++ typedata-math.py
@@ -2,10 +2,12 @@ type("teks apapun") #ini type data string/text
 
 a = 12345
 type(a) #ini tipe data INTEGER / BILANGAN BULAT
+print(type(a)) # Hasil type data int
 
 b = 123.35
 type (b) #Ini tipe data FLOAT / BILANGAN RILL / DESIMAL
+print(type(b)) # Hasil type data float
 
 c = 0.00001 # ini sama dengan 0.00001
 print(c)
-print(type(c))
+print(type(c)) # Hasil type data float
