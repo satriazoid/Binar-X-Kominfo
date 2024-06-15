d3b2e9d1043683daa514ba066397c55253b1daf9
Zoid <satriaalfa77@gmail.com>
2024-06-15 20:28:38 +0700

    update

--- googlecollab.py
+++ googlecollab.py
@@ -2,3 +2,4 @@ print ("Hello world")
 nama_lengkap = input("Masukan nama:")
 domisili = input("Domisili:")
 print("Perkenalkan, nama saya{}, berasal dari {}".format(nama_lengkap, domisili))
+# Hasil input Perkenalkan, nama saya Satria, berasal dari bogor
