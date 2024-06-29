0c3a73d2dd296ea7c3f10e0c0bcc4c774f9fbe8c
Zoid <satriaalfa77@gmail.com>
2024-06-29 19:38:31 +0700

    add list

---
+++ list.py
@@ -0,0 +1,11 @@
+# List dengan nilai kosong
+my_list = []
+print(my_list)
+
+# List dengan nilai integer dan string
+my_list = [1, "toni", 3]
+print(my_list)
+
+# Tambah data
+my_list.append(4)
+print(my_list)
